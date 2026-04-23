dd601c17dcc89ddbab3382dae68874002bfe0b25
Pranto78 <fshahriyar44@gmail.com>
2026-04-24 01:49:54 +0600

    Added the font sans

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,19 +8,15 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1  bg-background">
-      <Text className="text-xl font-bold text-accent">
+      <Text className="text-3xl font-sans-extrabold text-accent">
         Welcome to Nativewind!
       </Text>
 
-      <Link className="mt-4 bg-primary p-5 rounded-2xl text-white" href={"/onboarding"}>Go to Onboarding</Link>
-      <Link className="mt-4 bg-primary p-5 rounded-2xl text-white" href={"/(auth)/sign_in"}>Sign In</Link>
-      <Link className="mt-4 bg-primary p-5 rounded-2xl text-white" href={"/(auth)/sign_up"}>Sign Up</Link>
+      <Link className="mt-4 font-sans-bold bg-primary p-5 rounded-2xl text-white" href={"/onboarding"}>Go to Onboarding</Link>
+      <Link className="mt-4 font-sans-bold bg-primary p-5 rounded-2xl text-white" href={"/(auth)/sign_in"}>Sign In</Link>
+      <Link className="mt-4 font-sans-bold bg-primary p-5 rounded-2xl text-white" href={"/(auth)/sign_up"}>Sign Up</Link>
     
-      <Link href={{
-        pathname:"/subscription/[id]",
-        params:{id:"claude"},
-      }}> Claude Subscription</Link>
-    
+     
 
     </SafeAreaView>
   );
